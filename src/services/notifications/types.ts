20e79866b4017eef42746547300796d626fdd98e
JonatanFD <u20211b995@upc.edu.pe>
2026-02-16 21:03:46 -0500

    feat: fix enum for NotificationType

--- src/services/notifications/types.ts
+++ src/services/notifications/types.ts
@@ -3,10 +3,15 @@ export type NotificationStatus = "UNREAD" | "READ" | "ARCHIVED";
 export interface UpdateNotificationStatusRequest {
     newStatus: NotificationStatus;
 }
+export enum NotificationType {
+    INVITATION = "INVITATION",
+    REMINDER = "REMINDER",
+    ALERT = "ALERT",
+}
 
 export interface NotificationResponse {
     notificationId: string;
-    type: string;
+    type: NotificationType;
     message: string;
     senderEmail: string;
     status: string;
